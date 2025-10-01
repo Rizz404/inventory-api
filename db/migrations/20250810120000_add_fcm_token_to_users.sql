c8e1d407b3f126ddb28833d92c7165910d5fe544
Rizqiansyah Ramadhan <rizzthenotable@gmail.com>
2025-10-01 23:12:32 +0700

    feat(notification): add fcm token to user table

--- db/migrations/20250810120000_add_fcm_token_to_users.sql
+++
@@ -1,6 +0,0 @@
--- Add FCM token column to users table
-ALTER TABLE users
-ADD COLUMN fcm_token TEXT;
-
--- Add comment to explain the column
-COMMENT ON COLUMN users.fcm_token IS 'Firebase Cloud Messaging token for push notifications';
